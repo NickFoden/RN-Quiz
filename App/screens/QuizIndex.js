b862f53a5a81a5f1a8d7b7b819c7894122160024
Nick Foden <nickfoden@gmail.com>
2019-05-20 14:42:22 -0400

    Adding react navigation and quiz index

--- App/screens/QuizIndex.js
+++ App/screens/QuizIndex.js
@@ -2,8 +2,8 @@ import React from "react";
 
 import { View, Button } from "react-native";
 
-export default ({children}) => (
-    <View style={{flex:1, alignItems: "center", justifyContent:"center"}}>
-        {children}
-    </View>
-)
+export default ({ children }) => (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Button title="Go to next screen" onPress={() => alert("to do")} />
+  </View>
+);
